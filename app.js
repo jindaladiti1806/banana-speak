add5db1b57046e9918abfda5322a3cba2af86624
Aditi jindal <53328553+jindaladiti1806@users.noreply.github.com>
2020-12-06 17:28:22 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var btnTranslate = document.querySelector("#btn-translate")
+
+console.log(btnTranslate)
+
+btnTranslate.addEventListener("Click", function clickEventHandler(){
+    console.log("clicked")
+})
